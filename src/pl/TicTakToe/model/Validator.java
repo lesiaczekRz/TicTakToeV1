add61531997a8f54cd4d2f300d5d919efed6d7ef
Adam <panasiuk.adam@gmail.com>
2021-07-21 15:17:10 +0200

    Update Validator.java

--- src/pl/TicTakToe/model/Validator.java
+++ src/pl/TicTakToe/model/Validator.java
@@ -18,20 +18,20 @@ public class Validator {
         } else if (type == 'Y') {
             search = "[123]";
         }
-        Pattern compiledPattern = Pattern.compile(search);
-        Matcher matcher = compiledPattern.matcher(String.valueOf(x));
+        Pattern compiledPattern = Pattern.compile(search);  // jest taki żart programistyczny: miałem problem, postanowiłem rozwiązać go przy pomocy regexa. Teraz mam dwa problemy.
+        Matcher matcher = compiledPattern.matcher(String.valueOf(x)); // zamiast używać regexa, wrzuć ABC i 123 do dwóch tablic i użyj ArrayUtils.contains()
         if (!matcher.find()) {
             throw new MyNewException("Podano nieprawidłowy znak: " + x);
         }
     }
 
-    /**
+    /** // ta metoda wygląda mi jakby dotyczyła planszy, powinna być zawarta w Board
      * Sprawdza czy w tym polu już istnieje jakaś wartość
      *
      * @param x          int
      * @param y          int
      */
-    static void checkIfExists(int x, int y) throws MyNewException {
+    static void checkIfExists(int x, int y) throws MyNewException { // czy ta metoda musi rzucać wyjątkiem? nie wystarczy by zwracała true/false?
         if (!(BoardModel.ARRAY_BOARD[y][x] == null)) {
             throw new MyNewException("W tym polu już istnieje wartość");
         }
