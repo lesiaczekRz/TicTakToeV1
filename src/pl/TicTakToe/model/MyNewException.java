50a4ecc969a647d3b9b19214d70605b67fcd47a3
Adam <panasiuk.adam@gmail.com>
2021-07-21 14:53:52 +0200

    Update MyNewException.java

--- src/pl/TicTakToe/model/MyNewException.java
+++ src/pl/TicTakToe/model/MyNewException.java
@@ -1,6 +1,6 @@
 package pl.TicTakToe.model;
 
-public class MyNewException extends Exception {
+public class MyNewException extends Exception { // Ten wyjątek powinien mieć jakąś bardziej deskryptywną nazwę. W jakich sytuacjach się go rzuca?
     public MyNewException(String errorMessage) {
         super(errorMessage);
     }
