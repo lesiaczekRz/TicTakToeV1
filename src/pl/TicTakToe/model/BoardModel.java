89e5fcaf5956f30404c09d9fa837cf69bb6ece33
Adam <panasiuk.adam@gmail.com>
2021-07-21 14:51:48 +0200

    Review BoardModel
    Added comments for BoardModel class.

--- src/pl/TicTakToe/model/BoardModel.java
+++ src/pl/TicTakToe/model/BoardModel.java
@@ -3,11 +3,11 @@ package pl.TicTakToe.model;
 public class BoardModel implements BoardSettable {
     public final static int HEIGHT = 3;
     public final static int WIDTH = 3;
-    public final static String[][] ARRAY_BOARD = new String[HEIGHT][WIDTH];
+    public final static String[][] ARRAY_BOARD = new String[HEIGHT][WIDTH];     // To nie powinno być static. Lepiej zrobić do tego gettera i tak się do tego dostawać
     private int paramX, paramY;
 
     @Override
-    public void setCoordinates(CharacterType character, char x, char y) {
+    public void setCoordinates(CharacterType character, char x, char y) { // czemu x i y to są char a nie int?
         if (!checkForErrors(x, y)) {
             BoardModel.ARRAY_BOARD[paramY][paramX] = String.valueOf(character.getValue());
         }
@@ -16,13 +16,13 @@ public class BoardModel implements BoardSettable {
     /**
      * Sprawdza błędy
      *
-     * @param x         char
-     * @param y         char
+     * @param x         char // czemu char a nie int?
+     * @param y         char // czemu char a nie int?
      * @return boolean
      */
-    private boolean checkForErrors(char x, char y) {
+    private boolean checkForErrors(char x, char y) { 
         try {
-            Validator.checkCoordinate('X', x);
+            Validator.checkCoordinate('X', x); //ta metoda nic nie zwraca, czy nie interesuje nas wynik walidacji w tym miejscu?
             //noinspection SuspiciousNameCombination
             Validator.checkCoordinate('Y', y);
             paramX = convertX(x) - 1;
@@ -41,7 +41,7 @@ public class BoardModel implements BoardSettable {
      * @param x char
      * @return int
      */
-    private static int convertX(char x) {
+    private static int convertX(char x) { // char to pojedynczy znak z tablicy ASCII. Każdy znak ma swój unikalny kod, np A = 65, B=66 itp. Można zatem tę metodę sprowadzić do rzutowania char na int i odjęciu 64.
         return switch (x) {
             case 'A' -> 1;
             case 'B' -> 2;
